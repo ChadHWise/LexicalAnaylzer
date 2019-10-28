77b376def624e4a70f10cc7cbd5f3541da07da1b
Chad Wise <chadharrisonwise@hotmail.com>
2019-10-28 16:54:48 -0400

    updating switch statement

--- src/LexicalAnalyzer.java
+++ src/LexicalAnalyzer.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LexicalAnalyzer {
@@ -21,33 +22,101 @@ public class LexicalAnalyzer {
 	HashMap<String, Boolean> table = new HashMap<String, Boolean>();
 
 	public static void main(String[] args) throws IOException {
-		LexicalAnalyzer lex = new LexicalAnalyzer();
+		LexicalAnalyzer lexAn = new LexicalAnalyzer();
 		BufferedReader in = new BufferedReader(new FileReader("test.txt"));
 		char[] source = new char[100];
-		lex.loadKeywords();
-		int lineNo = 0;
+		lexAn.loadKeywords();
+		int lineNo = 1;
 		int state = 0;
-
-		Token total = lex.getToken("total");
-		lex.installId("price");
-		Token price = lex.getToken("price");
-		Token key = lex.getToken("if");
-
-		System.out.println(total.lexeme + " " + total.tokenType);
-		System.out.println(price.lexeme + " " + price.tokenType);
-		System.out.println(key.lexeme + " " + key.tokenType);
+		int forwardP = 0;
+//		Token total = lex.getToken("total");
+//		Token price = lex.getToken("price");
+//		Token key = lex.getToken("if");
+//
+//		System.out.println(total.lexeme + " " + total.tokenType);
+//		System.out.println(price.lexeme + " " + price.tokenType);
+//		System.out.println(key.lexeme + " " + key.tokenType);
 
 		String nextLine = in.readLine();
+		ArrayList<Token> tokens = new ArrayList<Token>();
+
 		while (nextLine != null) {
-			lineNo++;
 			source = nextLine.toCharArray();
-			for(int i = 0; i < source.length; i++) {
-				System.out.print(source[i]);
+			int lexStart = 0;
+			char forward = source[0];
+			for (int i = 0; i < source.length; i++) {
+				forwardP++;
+				switch (state) {
+				case 0:
+					if (Character.isAlphabetic(forward)) {
+						state = 1;
+						break;
+					} else if (Character.isDigit(forward)) {
+						state = 3;
+						break;
+					} else {
+						switch (forward) {
+						case ' ':
+							break;
+						case '\t':
+							break;
+						case '=':
+							state = 5;
+							break;
+						case '!':
+							state = 8;
+							break;
+						case '<':
+							state = 11;
+							break;
+						case '>':
+							state = 14;
+							break;
+						case '+':
+							state = 17;
+							break;
+						case '-':
+							state = 18;
+							break;
+						case '*':
+							state = 19;
+							break;
+						case '/':
+							state = 20;
+							break;
+						case '(':
+							state = 21;
+							break;
+						case ')':
+							state = 22;
+							break;
+						case '{':
+							state = 23;
+						case '}':
+							state = 24;
+
+						}
+					}
+				case 1:
+					if (Character.isAlphabetic(forward) || Character.isDigit(forward)) {
+						break;
+					} else {
+						state = 2;
+						break;
+					}
+				case 2:
+					String lex = "";
+					for (int j = lexStart; j <= --forwardP; j++) {
+						lex = lex + source[j];
+					}
+					tokens.add(lexAn.getToken(lex));
+					System.out.println(lexAn.getToken(lex).toString());
+				}
+				forward = source[forwardP];
 			}
-			
-			
-			
+
 			nextLine = in.readLine();
+			lineNo++;
 		}
 		in.close();
 
@@ -63,20 +132,14 @@ public class LexicalAnalyzer {
 	}
 
 	public Token getToken(String value) {
-		
-		
-		if(table.get(value) == null) {
-			return null;
-		}else if (table.get(value)) {
-			return new Token(100, value);
-		} else {
-			return new Token(111, value);
-		} 
-	}
 
-	public void installId(String value) {
 		if (table.get(value) == null) {
 			table.put(value, false);
+			return getToken(value);
+		} else if (table.get(value)) {
+			return new Token(111, value);
+		} else {
+			return new Token(100, value);
 		}
 	}
 
@@ -88,6 +151,10 @@ public class LexicalAnalyzer {
 		public Token(int tokenType, String lexeme) {
 			this.tokenType = tokenType;
 			this.lexeme = lexeme;
+		}
+
+		public String toString() {
+			return this.tokenType + " " + this.lexeme;
 		}
 
 	}
